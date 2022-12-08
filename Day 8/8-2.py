11be6d51de553b4b175fd59262a0292fd11532ed
Charlie Mawer <cmawer97@gmail.com>
2022-12-08 18:57:17 +0000

    Delete unused code for day 8-2

--- Day 8/8-2.py
+++ Day 8/8-2.py
@@ -1,32 +1,6 @@
 with open("input.txt") as f:
     data = [list(map(int, list(row))) for row in f.read().splitlines()]
 
-total = 0
-
-
-def check_tree_visible(data, x, y):
-    tree_height = data[y][x]
-    # print("Checking tree of height %i at co-ordinates (%i,%i)" % (tree_height, x, y))
-    # Perimeter trees are always visible
-    if x == 0 or x == len(data[0]) - 1:
-        return True
-    elif y == 0 or y == len(data) - 1:
-        return True
-    else:
-        # Left
-        if tree_height > max(data[y][:x]):
-            return True
-        # Right
-        if tree_height > max(data[y][x + 1 :]):
-            return True
-        # Top
-        if tree_height > max([row[x] for row in data[:y]]):
-            return True
-        # Bottom
-        if tree_height > max([row[x] for row in data[y + 1 :]]):
-            return True
-    return False
-
 
 def check_tree_score(data, x, y):
     tree_height = data[y][x]
